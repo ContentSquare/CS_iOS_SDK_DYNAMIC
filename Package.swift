89957b35aad173b59f74ebe39a706f9f9303560a
Boris Belaj <boris.belaj@contentsquare-ext.com>
2025-01-22 14:18:45 +0100

    repository setup

---
+++ Package.swift
@@ -0,0 +1,44 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CS_iOS_SDK",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "ContentsquareModule",
+            targets: ["ContentsquareModuleWrapper"]),
+    ],
+    dependencies: [
+        .package(
+            name: "CSSwiftProtobuf",
+            url: "https://github.com/ContentSquare/CSSwiftProtobuf.git",
+            .exact("1.28.2")),
+        .package(
+            name: "CSCrashReporter",
+            url: "https://github.com/ContentSquare/CS_iOS_CrashReporter.git",
+            .exact("1.0.1")),
+    ],
+    targets: [
+        // binaryTarget doesn't support dependency, use a wrapper to fix this.
+        .target(
+            name: "ContentsquareModuleWrapper",
+            dependencies: [
+                .target(name: "ContentsquareModule"),
+                "CSSwiftProtobuf",
+                "CSCrashReporter"
+            ],
+            path: "ContentsquareModuleWrapper",
+            resources: [
+                .process("ContentsquareBundle.bundle"),
+                .copy("PrivacyInfo.xcprivacy")
+            ]
+        ),
+        .binaryTarget(
+            name: "ContentsquareModule",
+            url: "https://github.com/ContentSquare/CS_iOS_SDK/releases/download/4.39.0/ContentsquareModuleSwiftPackageDynamic.xcframework.zip",
+            checksum: "261f2cecaf5c170f45da437f66de0f31bda28d1d1055819eeaa27abd834f92f5"),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,8 @@
+[Contentsquare](https://contentsquare.com) SDK for native iOS apps.
+Please refer to [the documentation for integration instructions](https://docs.contentsquare.com/ios/).
+
+---
+You can find more information about Contentsquare here:
+ - [Web tracking](https://docs.contentsquare.com/webtracking-impl-en/)
+ - [Android SDK](https://docs.contentsquare.com/android/)
+ - [React Native SDK](https://docs.contentsquare.com/react-native/)
