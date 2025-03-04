72880b93ef6e383398ab5c196c6641a91a593807
borisbelaj-cs <150795979+borisbelaj-cs@users.noreply.github.com>
2025-03-04 15:18:01 +0100

    Bump version to 4.40.2 (#3)

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ContentsquareModule",
-            url: "https://github.com/ContentSquare/CS_iOS_SDK/releases/download/4.39.0/ContentsquareModulePodDynamic.xcframework.zip",
-            checksum: "261f2cecaf5c170f45da437f66de0f31bda28d1d1055819eeaa27abd834f92f5"),
+            url: "https://github.com/ContentSquare/CS_iOS_SDK/releases/download/4.40.2/ContentsquareModulePodDynamic.xcframework.zip",
+            checksum: "41b04863bee790f583cb19dadf1d3c0dfa87a26f7ac0cbc32c7ff20cb3c7f5bb"),
     ]
 )
