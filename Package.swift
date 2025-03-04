b15ea3a536826668c1b170a9a8d54f43ad4d2219
Boris Belaj <boris.belaj@contentsquare-ext.com>
2025-03-04 15:34:51 +0100

    Revert to 4.40.0

--- Package.swift
+++ Package.swift
@@ -38,7 +38,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "ContentsquareModule",
-            url: "https://github.com/ContentSquare/CS_iOS_SDK/releases/download/4.40.2/ContentsquareModulePodDynamic.xcframework.zip",
+            url: "https://github.com/ContentSquare/CS_iOS_SDK/releases/download/4.40.0/ContentsquareModulePodDynamic.xcframework.zip",
             checksum: "41b04863bee790f583cb19dadf1d3c0dfa87a26f7ac0cbc32c7ff20cb3c7f5bb"),
     ]
 )
